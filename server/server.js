fca83756e175d40a0689525801e6dbd901a6499d
Karishma Kumar <karishma.connect@gmail.com>
2021-03-27 17:36:57 -0700

    add data and set up server on port 8080

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+const activityRoutes = require('./routes/activityRoutes');
+const userRoutes = require('./routes/userRoutes');
+const cors = require('cors');
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use('/public', express.static('public'));
+
+app.use('/activities', activityRoutes);
+app.use('/user', userRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+});
