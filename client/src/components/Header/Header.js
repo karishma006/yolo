5e4d47df3ab7f046026a16ed36f006eef6103a44
Karishma Kumar <karishma.connect@gmail.com>
2021-04-22 06:29:56 -0700

    Phase 2 commit

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
                 <h1>YOLO</h1>
             </div>
             <nav className='header__nav'>
-                <NavLink exact to='/' className='header__link' activeClassName='header__link--active'>Popular Activities</NavLink>
+                <NavLink exact to='/' className='header__link' activeClassName='header__link--active'>Trending</NavLink>
                 <NavLink to='/mybucket' className='header__link' activeClassName='header__link--active'>My Bucket</NavLink>
             </nav>
         </header>
